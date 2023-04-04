cdc5caaa3cccfd836afbd438ad95e272255ea4ea
HassanAriz <arizhassan@hotmail.fr>
2023-04-04 15:56:15 +0200

    First page

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+
+class DefaultController
+{
+  public function index()
+  {
+    return new Response('<h1>Hello World !</h1>');
+  }
+}
